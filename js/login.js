f5ec408ec79455a1d3fa77bde0ab06fe68727c29
hyunjea Ann <hyunjeaann@hyunjeaui-MacBookAir.local>
2021-12-09 15:33:43 +0900

    sync with westagram_frontend

--- js/login.js
+++ js/login.js
@@ -1,13 +1,42 @@
+// 1차 리팩토링후
 window.onload = function(){
-    let body = document.querySelector('body');
-    let button = document.querySelector('button')
+    let userName = document.querySelector('.username');
+    let userPassword = document.querySelector('.userpassword');
+    let button = document.querySelector('button');
+    let btnActivated = false;
 
-    body.addEventListener('keyup', function(){
+    let activateBtn = (e) => {
         let userName = document.querySelector('.username').value;
         let userPw = document.querySelector('.userpassword').value;
-        if (userName && userPw) {
+
+        if (userName && (userName.indexOf('@') !== -1) && (userPw.length >= 5)) {
             button.style.backgroundColor = '#0095F6';
             button.setAttribute('onclick', "location.href='main.html'");
+            btnActivated = true;
+        } else {
+            button.style.backgroundColor = '#c5e1fa';
+            button.setAttribute('onclick', "");
+            btnActivated = false;
         }
-    });
+
+        if (e.keyCode === 13 && btnActivated) location.href="main.html";
+    }
+
+    userName.addEventListener('keyup', activateBtn);
+    userPassword.addEventListener('keyup', activateBtn);
 }
+
+// 리팩토링전
+// window.onload = function(){
+//     let body = document.querySelector('body');
+//     let button = document.querySelector('button')
+
+//     body.addEventListener('keyup', function(){
+//         let userName = document.querySelector('.username').value;
+//         let userPw = document.querySelector('.userpassword').value;
+//         if (userName && userPw) {
+//             button.style.backgroundColor = '#0095F6';
+//             button.setAttribute('onclick', "location.href='main.html'");
+//         }
+//     });
+// }
--- js/main.js
+++ js/main.js
@@ -1,18 +1,25 @@
+// 1차 리팩토링후
 window.onload = function() {
-    let body = document.querySelector('body');
     let inputUpload = document.querySelector('.input_upload');
     let btnUpload = document.querySelector('.btn_upload');
     let contentFlag = false;
     let comments = document.querySelector('.comments');
+    let hearts = document.querySelectorAll('.hearts');
 
-    const addComment = (el) => {
-        el.innerHTML = `<strong>username </strong>${inputUpload.value}<small> 방금전</small>`;
-        if (contentFlag) {
-            comments.appendChild(el);
-        }
-    }
+    // 댓글좋아요기능 / 삭제기능
+    comments.addEventListener("click", (e) => {
+        console.log(e.target);
+        if (e.target.classList.contains('hearts')) { // 좋아요기능
+            e.target.classList.toggle("red");
+            if (e.target.className.startsWith("far")) e.target.className = e.target.className.replace("far", "fas");
+            else if (e.target.className.startsWith("fas")) e.target.className = e.target.className.replace("fas", "far");
+        } else if (e.target.classList.contains('fa-times-circle')) e.target.parentNode.remove();
+    });
 
-    body.addEventListener("keyup", function() {
+    // 댓글 제거기능
+
+    // 게시버튼 활성화 && 내용포함여부확인 기능
+    inputUpload.addEventListener("keydown", function() {
         if (inputUpload.value) {
             btnUpload.style.color = '#3e99ed';
             contentFlag = true;
@@ -22,26 +29,59 @@ window.onload = function() {
         }
     });
 
-    // btnUpload.addEventListener("click", function() {
-    //     addComment(document.createElement('span'));
-    // });
+    const addComment = (el) => {
+        el.innerHTML = `<strong>username </strong>${inputUpload.value}<small> 방금전</small><i class="fas fa-times-circle circles"></i><i class="far fa-heart hearts"></i>`;
+        if (!contentFlag) return;
 
-    // inputUpload.addEventListener("keyup", function(e) {
-    //     if (e.keyCode === 13) addComment(document.createElement('span'));
-    // });
+        comments.appendChild(el);
+        inputUpload.value = "";
+
+        hearts = document.querySelectorAll('.hearts');
+    }
 
     const handleComment = (event) => {
-        console.dir(event.target);
         const eventClassName = event.target.className;
 
-        if(eventClassName==='btn_upload') addComment(document.createElement('span'));
-        if(eventClassName==='input_upload') {
+        if(eventClassName === 'btn_upload') addComment(document.createElement('span'));
+        if(eventClassName === 'input_upload') {
             if(event.keyCode === 13) addComment(document.createElement('span'));
         }
     }
 
     btnUpload.addEventListener("click", handleComment);
-
     inputUpload.addEventListener("keyup", handleComment);
-
 }
+
+// 리팩토링전
+// window.onload = function() {
+//     let body = document.querySelector('body');
+//     let inputUpload = document.querySelector('.input_upload');
+//     let btnUpload = document.querySelector('.btn_upload');
+//     let contentFlag = false;
+//     let comments = document.querySelector('.comments');
+
+//     const addComment = (el) => {
+//         el.innerHTML = `<strong>username </strong>${inputUpload.value}<small> 방금전</small>`;
+//         if (contentFlag) {
+//             comments.appendChild(el);
+//         }
+//     }
+
+//     inputUpload.addEventListener("keyup", function() {
+//         if (inputUpload.value) {
+//             btnUpload.style.color = '#3e99ed';
+//             contentFlag = true;
+//         } else {
+//             btnUpload.style.color = '#c5e2fa';
+//             contentFlag = false;
+//         }
+//     });
+
+//     btnUpload.addEventListener("click", function() {
+//         addComment(document.createElement('span'));
+//     });
+
+//     inputUpload.addEventListener("keyup", function(e) {
+//         if (e.keyCode === 13) addComment(document.createElement('span'));
+//     });
+// }
